32426dc50f55ecd316fd1fffb86df937090eecd0
Piotr Mazur <peter.mazur05@gmail.com>
2025-06-11 14:38:29 -0400

    Created main and Student classes, added basic instance variables

---
+++ Student.java
@@ -0,0 +1,11 @@
+class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
