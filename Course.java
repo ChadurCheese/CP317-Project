f4e0a4ee3aaa0b9c5d7746bf0546458ee0d27e86
Piotr Mazur <peter.mazur05@gmail.com>
2025-07-04 21:43:34 -0400

    Course class created to store courses with grades

---
+++ Course.java
@@ -0,0 +1,33 @@
+class Courses extends Student{
+    private String code;
+    private double test1, test2, test3, exam, finalGrade;
+    
+    public Courses(int id, String name, String code, 
+                       double test1, double test2, double test3, double exam) {
+        super(id, name);
+        this.code = code;
+        this.test1 = test1;
+        this.test2 = test2;
+        this.test3 = test3;
+        this.exam = exam;
+        calculateFinalGrade();
+    }
+
+    private void calculateFinalGrade() {
+        finalGrade = (test1 + test2 + test3) * 0.2 + (exam * 0.4);
+    }
+
+    public String getCourseCode() {
+        return code;
+    }
+
+    public double getFinalGrade() {
+        return finalGrade;
+    }
+
+    @Override
+    public String toString() {
+        // return String.format(name, null)
+        return "";
+    }
+}
